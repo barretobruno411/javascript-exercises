f4fd1e725c80e982092b52e2684e383a3043d2bd
Bruno Duarte <bruno.duarte411@gmail.com>
2022-10-06 15:37:00 -0300

    Add 02 solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function (string, times) {
+  let result = "";
 
+  if (times === 0) return result
+  if (times < 0) return "ERROR"
+  
+  for (let i = 1; i <= times; i++) {
+    result += string
+  };
+
+  return result;
 };
+console.log(repeatString("hey",-1));
 
 // Do not edit below this line
 module.exports = repeatString;
