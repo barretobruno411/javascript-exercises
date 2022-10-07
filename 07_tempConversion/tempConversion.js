e3ec4d1896b062ea926185e433578354f3b5cbee
Bruno Duarte <bruno.duarte411@gmail.com>
2022-10-07 01:10:48 -0300

    Add 07 solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(Fahrenheit) {
+  let celsius = (Fahrenheit - 32) * 5 / 9
+  return parseFloat(celsius.toFixed(1))
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  let Fahrenheit = (celsius * 1.8) + 32
+  return parseFloat(Fahrenheit.toFixed(1))
 };
 
 // Do not edit below this line
