1a1e71649d11fed4a1752d763b480f985c3ff07e
Bruno Duarte <bruno.duarte411@gmail.com>
2022-10-07 00:19:23 -0300

    Add 05 solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,32 @@
-const sumAll = function() {
+const sumAll = function (numberOne, numberTwo) {
+  if (
+    typeof numberOne !== "number" ||
+    typeof numberTwo !== "number" ||
+    numberOne < 0 ||
+    numberTwo < 0
+  ) {
+    return "ERROR";
+  }
 
+  let sum = 0;
+
+  if (numberOne < numberTwo) {
+    for (let i = numberOne; i <= numberTwo; i++) {
+      console.log(i);
+      sum += i;
+      console.log("sum: " + sum);
+    }
+  } else {
+    for (let i = numberTwo; i <= numberOne; i++) {
+      console.log(i);
+      sum += i;
+      console.log("sum: " + sum);
+    }
+  }
+
+  return sum;
+  // for (let i = numberOne; )
 };
-
+console.log(sumAll(5, 1));
 // Do not edit below this line
 module.exports = sumAll;
