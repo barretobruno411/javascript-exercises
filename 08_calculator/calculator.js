e6aa9f3bc8e28d070af72b6d69a1ba39bb10a1ca
Bruno Duarte <bruno.duarte411@gmail.com>
2022-10-12 12:04:46 -0300

    Add comments on 08_CALCULATOR/calculator.js file

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,3 +1,4 @@
+
 const add = function(number1, number2) {
 	return number1 + number2
 };
@@ -5,11 +6,11 @@ const add = function(number1, number2) {
 const subtract = function(number1, number2) {
 	return number1 - number2
 };
-
+// sum of all values of an array
 const sum = function(array) {
 	return array.reduce((total, currentValue) => total += currentValue, 0)
 };
-
+// multiply all values of an array
 const multiply = function(array) {
   return array.reduce((total, currentValue) => total *= currentValue, 1)
 };
@@ -17,7 +18,7 @@ const multiply = function(array) {
 const power = function(number1, number2) {
 	return number1 ** number2
 };
-
+//return the factorial of a number
 const factorial = function(number) {
 	const array = []
 
@@ -27,7 +28,6 @@ const factorial = function(number) {
 
   return array.reduce((total, prev) => total *= prev, 1)
 };
-console.log(factorial(10))
 // Do not edit below this line
 module.exports = {
   add,
