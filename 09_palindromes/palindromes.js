22187da9ee0e934840759a5ab25e9d247655b1ce
Bruno Duarte <bruno.duarte411@gmail.com>
2022-10-12 12:44:00 -0300

    Add 09 palidrome solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    const initial = string.replace(/[^\w\s]|( )+/gi, '').toLowerCase()
+    const resume = initial.split("").reverse().join("")
+    return initial === resume
 };
-
+console.log(palindromes("A car, a man, a maraca."))
 // Do not edit below this line
 module.exports = palindromes;
