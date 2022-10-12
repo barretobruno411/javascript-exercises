febaf0cfe0c29a5d23fafdf6999489c1e4d063be
Bruno Duarte <bruno.duarte411@gmail.com>
2022-10-12 11:44:32 -0300

    Add 08 calculator solutions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,27 +1,33 @@
-const add = function() {
-	
+const add = function(number1, number2) {
+	return number1 + number2
 };
 
-const subtract = function() {
-	
+const subtract = function(number1, number2) {
+	return number1 - number2
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, currentValue) => total += currentValue, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, currentValue) => total *= currentValue, 1)
 };
 
-const power = function() {
-	
+const power = function(number1, number2) {
+	return number1 ** number2
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(number) {
+	const array = []
 
+  for (let i = number; i >= 1; i--) {
+    array.push(i)
+  }
+
+  return array.reduce((total, prev) => total *= prev, 1)
+};
+console.log(factorial(10))
 // Do not edit below this line
 module.exports = {
   add,
