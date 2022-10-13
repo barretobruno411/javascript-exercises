3e32892fe4ffe5e629b1404adfb0c6961852878c
Bruno Duarte <bruno.duarte411@gmail.com>
2022-10-13 10:19:30 -0300

    Add 11 getTHeTitles solution

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,18 @@
-const getTheTitles = function() {
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
 
+const getTheTitles = function(books) {
+    const result = books.map(item => item["title"])
+    return result
 };
-
+console.log(getTheTitles(books))
 // Do not edit below this line
 module.exports = getTheTitles;
